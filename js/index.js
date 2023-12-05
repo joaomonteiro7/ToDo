f820d2912857dd1772117813b757ab5e13496391
joaomonteiro7 <114528973+joaomonteiro7@users.noreply.github.com>
2023-12-05 00:58:43 +0000

    delete & check task done
    create 2 functions
    1 - delete task when click on "x" icon
    2 - mark task as done or no with the text decoration

--- js/index.js
+++ js/index.js
@@ -1,42 +1,60 @@
-window.addEventListener("load", ()=>{
+window.addEventListener("load", () => {
+  const btnAddTask = document.querySelector("#addTask");
+  const taskContainer = document.querySelector("#taskContainer");
 
-    const btnAddTask = document.querySelector("#addTask")
-    const taskContainer = document.querySelector("#taskContainer")
+  btnAddTask.addEventListener("click", () => {
+    /* individual div for tasks */
+    const taskDiv = document.createElement("div");
+    taskDiv.setAttribute("id", "task");
 
+    /* create checkbox and give id*/
+    const taskCheckBox = document.createElement("input");
+    taskCheckBox.setAttribute("type", "checkbox");
+    taskCheckBox.setAttribute("id", "checkTaskDone");
+    taskDiv.appendChild(taskCheckBox);
 
-    btnAddTask.addEventListener("click", ()=>{
+    /* get text value form input */
+    const taskName = document.querySelector("#taskInput").value;
 
-        /* individual div for tasks */
-        const taskDiv = document.createElement("div")
-        taskDiv.setAttribute("id", "task")
+    /* create a label for task name */
+    const taskLabel = document.createElement("label");
+    /* give an id to the label */
+    taskLabel.setAttribute("class", "taskName");
+    /* write input value on label */
+    taskLabel.textContent = taskName;
+    /* add label to the container */
+    taskDiv.appendChild(taskLabel);
+    /* delete input text */
+    document.querySelector("#taskInput").value = "";
 
-        /* create checkbox and give id*/
-        const taskCheckBox = document.createElement("input")
-        taskCheckBox.setAttribute("type", "checkbox")
-        taskCheckBox.setAttribute("id", "checkTaskDone")
-        taskDiv.appendChild(taskCheckBox)
+    /* create span for icon from font awesome */
+    const iconDelete = document.createElement("span");
+    iconDelete.setAttribute("id", "taskDelete");
+    iconDelete.setAttribute("class", "fa-solid fa-xmark");
+    taskDiv.appendChild(iconDelete);
 
-        /* get text value form input */
-        const taskName = document.querySelector("#taskInput").value
-        
-        /* create a label for task name */
-        const taskLabel = document.createElement("label")
-        /* give an id to the label */
-        taskLabel.setAttribute("id", "taskName")
-        /* write input value on label */
-        taskLabel.textContent = taskName
-        /* add label to the container */
-        taskDiv.appendChild(taskLabel)
-        /* delete input text */
-        document.querySelector("#taskInput").value = ""
+    /* add the individual div to the task container */
+    taskContainer.appendChild(taskDiv);
+  });
 
-        /* create span for icon from font awesome */
-        const iconDelete = document.createElement("span")
-        iconDelete.setAttribute("id", "taskDelete")
-        iconDelete.setAttribute("class", "fa-solid fa-xmark")
-        taskDiv.appendChild(iconDelete)
+  document.body.addEventListener("click", () => {
+    const allTasks = document.querySelectorAll("#taskDelete");
+    for (let i = 0; i < allTasks.length; i++) {
+      allTasks[i].addEventListener("click", () => {
+        let parentNode = allTasks[i].parentElement; //get parent node of clicked element
+        parentNode.remove(); //delete that parent node
+      });
+    }
 
-        /* add the individual div to the task container */
-        taskContainer.appendChild(taskDiv)
-    })
-})
+    const allCheckBox = document.querySelectorAll("#checkTaskDone");
+    for (let i = 0; i < allCheckBox.length; i++) {
+      allCheckBox[i].addEventListener("change", () => {
+        /* get the next element my label with the text */
+        const taskLabel = allCheckBox[i].nextElementSibling;
+        taskLabel.style.textDecoration = allCheckBox[i].checked
+          ? "line-through"
+          : "none";
+      });
+    }
+  });
+});
