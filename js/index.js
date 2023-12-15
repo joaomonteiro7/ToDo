ae312d9ce0806b8ea39c24b46faf5f0145642b9f
joaomonteiro7 <114528973+joaomonteiro7@users.noreply.github.com>
2023-12-15 12:13:22 +0000

    some code changes
    add some new web styling
    comment the JS code

--- js/index.js
+++ js/index.js
@@ -3,33 +3,49 @@ window.addEventListener("load", () => {
   const taskContainer = document.querySelector("#taskContainer");
   let taskObj = {};
 
+  /* where i check if local sotorage have tasks 
+     and create the elements to show the values
+     on page
+  */
   if (localStorage.getItem("tasks") != null) {
+    /* getting the items from localStorage thar are saved in JSON format 
+       and transform into one obj
+    */
     taskObj = JSON.parse(localStorage.getItem("tasks"));
-    console.log(taskObj);
-    for (key in taskObj) {
+    Object.keys(taskObj).forEach((key) => {
       const taskDiv = document.createElement("div");
       taskDiv.setAttribute("id", "task");
 
+      /* create check box */
       const taskCheckBox = document.createElement("input");
       taskCheckBox.setAttribute("type", "checkbox");
       taskCheckBox.setAttribute("id", "checkTaskDone");
       taskCheckBox.checked = taskObj[key];
 
-      taskDiv.appendChild(taskCheckBox);
 
+      /* create label(task name) */
       const taskLabel = document.createElement("label");
       taskLabel.setAttribute("class", "taskName");
       taskLabel.textContent = key;
-      taskCheckBox.checked ? taskLabel.style.textDecoration = "line-through" :taskLabel.style.textDecoration = "none"
-      taskDiv.appendChild(taskLabel);
 
+      /* check if checkBox are checked or no to add text decoration on task name */
+      taskCheckBox.checked
+        ? (taskLabel.style.textDecoration = "line-through")
+        : (taskLabel.style.textDecoration = "none");
+
+      /* create icon delete using font awasome */
       const iconDelete = document.createElement("span");
       iconDelete.setAttribute("id", "taskDelete");
       iconDelete.setAttribute("class", "fa-solid fa-xmark");
+
+      /* add all elements into div that store all tasks */
+      taskDiv.appendChild(taskCheckBox);
+      taskDiv.appendChild(taskLabel);
       taskDiv.appendChild(iconDelete);
 
+      /* add task div into the task container that have all divs with all tasks */
       taskContainer.appendChild(taskDiv);
-    }
+    });
   }
 
   btnAddTask.addEventListener("click", () => {
@@ -45,64 +61,70 @@ window.addEventListener("load", () => {
 
     /* get text value form input */
     const taskName = document.querySelector("#taskInput").value;
-    if(taskName.length > 0 && taskName.length < 38){
-    /* create a label for task name */
-    const taskLabel = document.createElement("label");
-    /* give an id to the label */
-    taskLabel.setAttribute("class", "taskName");
-    /* write input value on label */
-    taskLabel.textContent = taskName;
-    /* add label to the container */
-    taskDiv.appendChild(taskLabel);
-    /* delete input text */
-    document.querySelector("#taskInput").value = "";
+    if (taskName.length > 0 && taskName.length < 38) {
+      /* create a label for task name */
+      const taskLabel = document.createElement("label");
+      /* give an id to the label */
+      taskLabel.setAttribute("class", "taskName");
+      /* write input value on label */
+      taskLabel.textContent = taskName;
 
-    /* create span for icon from font awesome */
-    const iconDelete = document.createElement("span");
-    iconDelete.setAttribute("id", "taskDelete");
-    iconDelete.setAttribute("class", "fa-solid fa-xmark");
-    taskDiv.appendChild(iconDelete);
+      /* add label to the container */
+      taskDiv.appendChild(taskLabel);
+      /* delete input text */
+      document.querySelector("#taskInput").value = "";
 
-    /* add the individual div to the task container */
-    taskContainer.appendChild(taskDiv);
-    saveTasks();
+      /* create span for icon from font awesome */
+      const iconDelete = document.createElement("span");
+      iconDelete.setAttribute("id", "taskDelete");
+      iconDelete.setAttribute("class", "fa-solid fa-xmark");
+      taskDiv.appendChild(iconDelete);
+
+      /* add the individual div to the task container */
+      taskContainer.appendChild(taskDiv);
+      saveTasks();
     }
-
   });
 
   document.body.addEventListener("click", () => {
     const allTasks = document.querySelectorAll("#taskDelete");
     for (let i = 0; i < allTasks.length; i++) {
-      allTasks[i].addEventListener("click", () => {
-        let parentNode = allTasks[i].parentElement; //get parent node of clicked element
-        parentNode.remove(); //delete that parent node\
-      saveTasks();
 
+      allTasks[i].addEventListener("click", () => {
+        /* get the parent where the incon delete clicked belongs and del all 
+           elements from parent of the incon delete clicked 
+        */
+        let parentNode = allTasks[i].parentElement; 
+        parentNode.remove();
+        saveTasks();
       });
     }
 
     const allCheckBox = document.querySelectorAll("#checkTaskDone");
     for (let i = 0; i < allCheckBox.length; i++) {
       allCheckBox[i].addEventListener("change", () => {
-        /* get the next element my label with the text */
+        /* get the next element after the checkBox 
+           checkBox -> sibling
+        */
         const taskLabel = allCheckBox[i].nextElementSibling;
+        /* when checkBox is clicked give text decoration to the task name(label) */
         taskLabel.style.textDecoration = allCheckBox[i].checked
-          ? "line-through"
-          : "none";
-      saveTasks();
-
+        ? "line-through"
+        : "none";
+        saveTasks();
       });
     }
   });
 
   function saveTasks() {
+    /* get all elements(uin this case is div) with the id task */
     const tasksDiv = document.querySelectorAll("#task");
     const taskObj = {};
     for (let i = 0; i < tasksDiv.length; i++) {
       const taskLabel = tasksDiv[i].querySelector("label");
       const taskCheckBox = tasksDiv[i].querySelector("input");
       taskObj[taskLabel.textContent] = taskCheckBox.checked;
-
+      /* saving the obj on localStorage with JSON format */
       const stringifiedObj = JSON.stringify(taskObj);
       localStorage.setItem("tasks", stringifiedObj);
     }
